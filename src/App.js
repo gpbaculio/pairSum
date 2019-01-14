4034bb74dc090ee117ce703c63030507b16f4a51
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-01-14 17:55:36 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    numbers: [],
+    equals: 0
+  };
+  onChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  onSubmit = async e => {
+    e.preventDefault();
+    const { numbers, equals } = this.state;
+    const splitNums = numbers.split(',').map(n => Number(n));
+    const results = [];
+    splitNums.forEach((i, idx) => {
+      splitNums
+        .filter(c => c !== i)
+        .forEach(x => {
+          const result = i + x;
+          console.log('result = ', result);
+          results.push(result);
+        });
+    });
+    if (results.includes(Number(equals))) {
+      alert('Pair Sum correct');
+    } else {
+      alert('No pair');
+    }
+  };
+
   render() {
+    const { numbers, equals } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <label className="form-label" htmlFor="email">
+            Numbers
+            <input
+              type="text"
+              id="numbers"
+              name="numbers"
+              value={numbers}
+              onChange={this.onChange}
+            />
+          </label>
+        </div>
+
+        <div className="form-group">
+          <label className="form-label" htmlFor="password">
+            Equals
+            <input
+              type="number"
+              id="equals"
+              name="equals"
+              value={equals}
+              onChange={this.onChange}
+            />
+          </label>
+        </div>
+
+        <button
+          disabled={!numbers || !equals}
+          type="submit"
+          color="primary"
+          className="btn-block">
+          Submit
+        </button>
+      </form>
     );
   }
 }
